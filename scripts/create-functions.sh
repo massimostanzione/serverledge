4b7b665fe88badf862110b3ae609e39d9c44f76d
EnricoDAlessandro97UNI <enrico.dalessandro@alumni.uniroma2.eu>
2024-08-29 10:49:44 +0200

    Sistemata la create-functions.sh

--- scripts/create-functions.sh
+++ scripts/create-functions.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-bin/serverledge-cli create -f f1 --memory 512 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
-bin/serverledge-cli create -f f2 --memory 512 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
-bin/serverledge-cli create -f f3 --memory 128 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
-bin/serverledge-cli create -f f4 --memory 1024 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
-bin/serverledge-cli create -f f5 --memory 256 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
+#!/bin/sh
+
+HOST=192.168.122.21
+PORT=1324
+
+bin/serverledge-cli create -H $HOST -P $PORT -f f1 --memory 512 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
+bin/serverledge-cli create -H $HOST -P $PORT -f f2 --memory 512 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
+bin/serverledge-cli create -H $HOST -P $PORT -f f3 --memory 128 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
+bin/serverledge-cli create -H $HOST -P $PORT -f f4 --memory 1024 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
+bin/serverledge-cli create -H $HOST -P $PORT -f f5 --memory 256 --src examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
