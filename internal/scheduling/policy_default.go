5d290e299a4aba1e64424d84d8b3926502f778b0
EnricoDAlessandro97UNI <enrico.dalessandro@alumni.uniroma2.eu>
2024-09-11 17:41:22 +0200

    Test Pierpaolo

--- internal/scheduling/policy_default.go
+++ internal/scheduling/policy_default.go
@@ -70,6 +70,10 @@ func (p *DefaultLocalPolicy) OnCompletion(completed *scheduledRequest) {
 }
 
 func (p *DefaultLocalPolicy) OnArrival(r *scheduledRequest) {
+
+	log.Println("Warm status:", node.WarmStatus())
+	log.Println("Busy status:", node.BusyStatus())
+
 	containerID, err := node.AcquireWarmContainer(r.Fun)
 	if err == nil {
 		execLocally(r, containerID, true)
